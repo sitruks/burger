c34975f82597b6238834cf27d9b6600c7226ed41
SITRUK <kurtis.santillanes@gmail.com>
2019-06-04 07:54:07 -0700

    setup db and seed file through terminal

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 USE burgers_db;
 -- Insert a set of records.
 INSERT INTO burgers (burger_name) VALUES ('Royale with Cheese');
-INSERT INTO burgers (burger_name) VALUES ('');
+INSERT INTO burgers (burger_name) VALUES ('Triple Stack BLT');
