49edfac1565c80c63b89d47d3ff62ac9e26ca758
SITRUK <kurtis.santillanes@gmail.com>
2019-06-05 10:24:40 -0700

    testing different jawsdb path

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var connection;
-var JAWSDB_URL = "mysql://uvzh2hiya2we88ib:lud6v51q2bh2vktj@d6q8diwwdmy5c9k9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/z2gmtx18xro89ldl";
+// var JAWSDB_URL = "mysql://uvzh2hiya2we88ib:lud6v51q2bh2vktj@d6q8diwwdmy5c9k9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/z2gmtx18xro89ldl";
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
