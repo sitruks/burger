550cc05b79bedda812de680a36908e500ea0c3d8
SITRUK <kurtis.santillanes@gmail.com>
2019-06-05 10:21:12 -0700

    gui functionality added to the view. can now change boolean state of burgers in seed file.

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,38 @@
 var connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+  
+  // Helper function to convert object key/value pairs to SQL syntax
+  function objToSql(ob) {
+    var arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  };
+
 var orm = {
   selectAll: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
@@ -10,26 +43,41 @@ var orm = {
       cb(result);
     });
   },
-  insertOne: function(burger, callback) {
-    var s = "INSERT INTO " + tableName + " (burger_name, devoured) VALUES (?,?)";
-    burger.devoured = burger.devoured || 0;
-    connection.query(s, [
-      burger.burger_name, burger.devoured
-    ], function(err, result) {
+  insertOne: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
 
-      callback(result);
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
 
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
     });
   },
-  updateOne: function(burger, callback) {
-    var s = "UPDATE " + tableName + " SET burger_name=? WHERE id=?";
+  updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
 
-    connection.query(s, [
-      burger.burger_name, burger.id
-    ], function(err, result) {
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
 
-      callback(result);
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
 
+      cb(result);
     });
   }
 };
